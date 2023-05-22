a69d91a0fcfc8be263664b3877111cff13228957
Benjamin Verrill <benjaminverrill@Benjamins-MacBook-Pro.local>
2023-05-22 16:46:16 -0400

    Attempt at fixing image display

--- src/app/data.js
+++ src/app/data.js
@@ -12,7 +12,7 @@ export const projects = [
       subtitle: "React Native Japanese Quiz App",
       description:
       "Nihongo Vocab was a personal project that got it's start with my desire to want to learn Japanese. There are many language and learning apps out there, but I wanted something customized to how I learn best. It began as a simple flash card app, but I soon found myself adding more and more features. Users can shuffle their cards, start with the answer first, or use hints if they simply can't understand the Kanji. The app tracks the users progress, showing how well they've answered each Kaniji they've viewed so far or show a list of the ones that need the most practice. Random quiz buttons will start quizzes based only on Kanji the user has seen so far. Or, the user can use the Least Practiced buttons to get quizzed on the Kanji they need the most help with.",
-      image: ["/nihongovocab/IMG_8928.png", "/nihongovocab/IMG_8929.png", "/nihongovocab/IMG_8931.png"],
+      image: ["/nihongovocab/IMG_8928.PNG", "/nihongovocab/IMG_8929.PNG", "/nihongovocab/IMG_8931.PNG"],
       link: "https://github.com/TorturousTurtle/NihongoVocab",
     },
   ];
